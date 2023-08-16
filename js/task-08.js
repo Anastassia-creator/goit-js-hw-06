8e038b4394f55464378217747196f9b2d0285085
Anastassia-creator <118266539+Anastassia-creator@users.noreply.github.com>
2023-08-17 01:25:20 +0300

    hy

--- js/task-08.js
+++ js/task-08.js
@@ -3,12 +3,18 @@ const form = document.querySelector('.login-form')
 form.addEventListener('submit', (event) => {
    event.preventDefault();
    const formElements = event.currentTarget.elements;
-   console.log(formElements);
 
    let email = formElements.email.value;
    let password = formElements.password.value;
+   
+   const form = {
+    email: email,
+    password: password,
+   }
+ console.log (form);
 
 if (email === " " || password === " "){
     alert = "Всі поля повинні бути заповнені";
 }
+countReset()
 })  
